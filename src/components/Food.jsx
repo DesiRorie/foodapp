7bfcb3fffb110f16464cfb64c7821488ece09a4f
DesiRorie <desirorie@icloud.com>
2023-04-29 14:21:53 -0400

    .

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import StarIcon from "@mui/icons-material/Star";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import { Link } from "react-router-dom";
+import sushi from "../assets/california-roll.jpg";
+import noodles from "../assets/udonNoodles.jpeg";
 
 const Food = () => {
   return (
@@ -10,7 +12,7 @@ const Food = () => {
         <div className="ratingDiv">
           <StarIcon style={{ color: "gold" }} /> 5.0
         </div>
-        {/* <img /> */}
+        <img src={sushi} />
         <Link to="/foodpage" className="foodTitle">
           <h4>California Roll</h4>
         </Link>
@@ -23,6 +25,7 @@ const Food = () => {
         <div className="ratingDiv">
           <StarIcon style={{ color: "gold" }} /> 4.6
         </div>
+        <img src={noodles} />
         {/* <img /> */}
         <Link to="/foodpage" className="foodTitle">
           <h4>Udon Noodles</h4>
